7c7d2c8de0d760a30f7b5e1ff39535fb8b6e3f4e
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-04-25 18:25:10 +0900

    add: 新しいsupabaseのcrud処理

--- src/app/utils/supabaseFncUpdate.ts
+++ src/app/utils/supabaseFncUpdate.ts
@@ -3,7 +3,7 @@ import { Descript, Ingredient } from "../types";
 
 export async function updateRecipeImage(id: number, image_url: string) {
   const { error } = await supabase
-    .from("Recipes")
+    .from("recipes")
     .update({ image_url: image_url })
     .eq("id", id);
   if (error) {
@@ -11,23 +11,30 @@ export async function updateRecipeImage(id: number, image_url: string) {
   }
 }
 
-// Ingredientをupdateすることは今のところない
-// export async function UpdateIngredient(id: number, image_url: string) {
-//   const { error } = await supabase
-//     .from("Ingredients")
-//     .update({ image_url: image_url })
-//     .eq("id", id);
-// }
-export async function ExchengeIngredient(ing1: Ingredient, ing2: Ingredient) {
+export async function updateIngredient(
+  id: number,
+  name: string,
+  amount: string,
+  index: number
+) {
   const { error } = await supabase
-    .from("Ingredients")
+    .from("ingredients")
+    .update({ name: name, amount: amount, index: index })
+    .eq("id", id);
+  if (error) {
+    console.error("ingredientの更新中にエラー", error);
+  }
+}
+export async function exchangeIngredient(ing1: Ingredient, ing2: Ingredient) {
+  const { error } = await supabase
+    .from("ingredients")
     .update({
       name: ing2.name,
       amount: ing2.amount,
     })
     .eq("id", ing1.id);
   supabase
-    .from("Ingredients")
+    .from("ingredients")
     .update({
       name: ing1.name,
       amount: ing1.amount,
@@ -41,24 +48,32 @@ export async function ExchengeIngredient(ing1: Ingredient, ing2: Ingredient) {
 
 export async function updateDescriptsImage(id: number, image_url: string) {
   const { error } = await supabase
-    .from("Descripts")
+    .from("descripts")
     .update({ image_url: image_url })
     .eq("id", id);
   if (error) {
     console.error("supabaseエラー", error.message);
   }
 }
-
-export async function ExchengeDescripts(des1: Descript, des2: Descript) {
+export async function updateDescripts(id:number,text:string,index:number) {
   const { error } = await supabase
-    .from("Descripts")
+    .from("descripts")
+    .update({ text:text, index: index })
+    .eq("id", id);
+  if (error) {
+    console.error("descriptsの更新中にエラー", error);
+  }
+}
+export async function exchangeDescripts(des1: Descript, des2: Descript) {
+  const { error } = await supabase
+    .from("descripts")
     .update({
       image_url: des2?.image_url,
       text: des2?.text,
     })
     .eq("id", des1.id);
   supabase
-    .from("Descripts")
+    .from("descripts")
     .update({
       image_url: des1?.image_url,
       text: des1?.text,
@@ -71,9 +86,8 @@ export async function ExchengeDescripts(des1: Descript, des2: Descript) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function arrayShuffle(array:any[]) {
-  for(let i = (array.length - 1); 0 < i; i--){
-
+export function arrayShuffle(array: any[]) {
+  for (let i = array.length - 1; 0 < i; i--) {
     // 0〜(i+1)の範囲で値を取得
     const r = Math.floor(Math.random() * (i + 1));
 
