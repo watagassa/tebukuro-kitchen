2ec5d9785a3071ea0e7aab19fde27eea7a7d6a73
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-04-30 18:38:40 +0900

    add:マイページの作成

---
+++ src/app/future_user_id/MyResipeItem.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+const MyResipeItem = () => {
+  return (
+    <section className="flex justify-between px-2 py-3 border-b-2">
+      <div className="flex items-start gap-3 w-full">
+        <div className="relative w-20 md:w-32 aspect-[5/4] shrink-0">
+          <Image
+            src="/image/thumbnail.png"
+            alt=""
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="flex flex-col justify-between flex-grow h-full">
+          <p className="font-semibold mt-auto text-sm break-words">
+            基本チャーハン
+          </p>
+          <div className="flex gap-4 text-xs self-end pt-1 md:pt-3">
+            <button className="bg-white text-orange-400 border-orange-400 border-2 px-4 py-1 rounded-3xl">
+              編集
+            </button>
+            <button className="bg-orange-400 text-white px-4 py-1 rounded-3xl">
+              削除
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyResipeItem;
--- src/app/future_user_id/page.tsx
+++ src/app/future_user_id/page.tsx
@@ -1,28 +1,44 @@
 import Footer from "@/app/conponents/Footer";
-import { FiUser } from "react-icons/fi";
+import MyResipeItem from "./MyResipeItem";
+import Image from "next/image";
 
 const UserId = () => {
   return (
     <div className="bg-[#FFFBF4] min-h-screen flex flex-col">
-      <header
-        className={`bg-white sticky w-full flex items-center justify-center p-6 border-b border-gray-400 shadow-md `}
-      >
-        <p
-          className={`text-center text-xl font-semibold pl-10 pr-4 text-balance text-black`}
-        >
+      <header className="bg-orange-400 sticky w-full flex items-center justify-center p-6 border-b border-gray-400 shadow-md">
+        <p className="text-center text-xl font-semibold pl-10 pr-4 text-balance text-white">
           マイページ
         </p>
       </header>
-      <section className="flex gap-x-3 p-3 flex-grow">
-        <div className="rounded-full bg-purple-500 size-10 flex items-center justify-center">
-          <FiUser size={24} stroke="#fff" />
+      <section className="flex p-3 bg-white justify-between">
+        <div className="flex gap-x-3">
+          <div className="relative size-16 shrink-0">
+            <Image
+              src="/image/thumbnail.png"
+              alt=""
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+          <div className="font-semibold my-auto">
+            <p className="pb-1 text-xl text-black">ゲストユーザー</p>
+            <p className="text-xs text-[#797575]">@1124awerq</p>
+          </div>
         </div>
-        <div className="text-[#797575] font-semibold">
-          <p className="pb-1">ゲストユーザー</p>
-          <p className="text-xs">@1124awerq</p>
-        </div>
+        <p className="relative shrink-0 my-auto bg-orange-400 text-white px-3 py-2 rounded-3xl text-xs font-semibold ml-auto mr-2">
+          ログアウト
+        </p>
       </section>
-      <Footer pathName="/future_user_id" />
+      <p className="font-semibold text-xl mt-8 mb-4 pl-2">マイレシピ一覧</p>
+      <section className="bg-white rounded-xl mx-2">
+        <MyResipeItem />
+        <p className="flex justify-center font-semibold p-8 text-gray-400">
+          保存されたレシピは以上です
+        </p>
+      </section>
+      <div className="mt-auto">
+        <Footer pathName="/future_user_id" />
+      </div>
     </div>
   );
 };
