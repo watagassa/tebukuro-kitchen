bb58e844f94fcd079951e67542744072f815ee09
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-05-09 22:58:17 +0900

    update:import文の並び替え update:import文を絶対パスに修正 update:コメントの修正

--- src/app/conponents/FavoriteButton.tsx
+++ src/app/conponents/FavoriteButton.tsx
@@ -1,50 +1,49 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { FiHeart } from "react-icons/fi";
+
 import { Recipe } from "@/app/types";
 import {
   addFavorites,
   isFavorited,
   deleteFavorites,
-} from "../utils/supabaseFunctionsNew";
-import { useEffect, useState } from "react";
-import { FiHeart } from "react-icons/fi";
+} from "@/app/utils/supabaseFunctionsNew";
 
 type FavoriteButtonProps = {
   recipe: Recipe;
 };
 
 const FavoriteButton = ({ recipe }: FavoriteButtonProps) => {
+  const recipe_id: number = recipe.id;
   const [loading, setLoading] = useState<boolean>(true);
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
-  const recipe_id: number = recipe.id; // パラメータのrecipe_idを取得
 
-  // レンダリング時にlocalStorageを確認し、記事が既にお気に入りにあるかを判定
+  // レンダリング時にDBを確認し、レシピが既にお気に入りにあるかを判定
   useEffect(() => {
     const checkFavorite = async () => {
-      const isFavo = await isFavorited(recipe_id)
+      const isFavo = await isFavorited(recipe_id);
       setIsFavorite(isFavo);
       setLoading(false);
-    }
+    };
     checkFavorite();
   }, [recipe_id]);
 
-  // クリックイベント: お気に入りの状態を切り替え、localStorageを更新
+  // クリックイベント: お気に入りの状態を切り替え、DBを更新
   const handleFavoriteClick = async () => {
     if (isFavorite) {
       // お気に入りから削除
-      await deleteFavorites(recipe_id)
+      await deleteFavorites(recipe_id);
     } else {
       // お気に入りに追加
-      await addFavorites(recipe_id)
+      await addFavorites(recipe_id);
     }
 
     // isFavoriteの状態を反転
     setIsFavorite(await isFavorited(recipe_id));
   };
-
-  if (loading) {
-    return null;
-  }
+  if (loading) return null;
 
   return (
     <div className="text-center space-y-1" onClick={handleFavoriteClick}>
