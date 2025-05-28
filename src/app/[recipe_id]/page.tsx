d6a45ef416eb5f6fbdba042201066a2d15885e68
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-05-28 21:46:54 +0900

    update:料理詳細画面のデザイン変更

--- src/app/[recipe_id]/page.tsx
+++ src/app/[recipe_id]/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 
-import DescriptItem from "@/app/conponents/DescriptItem";
-import FavoriteButton from "@/app/conponents/FavoriteButton";
-import Footer from "@/app/conponents/Footer";
-import IngredientItem from "@/app/conponents/IngredientItem";
-import RecipeHeader from "@/app/conponents/RecipeHeader";
-import { Descript, DetailRecipe, Ingredient } from "@/app/types";
-import { getDetailRecipebyId } from "@/app/utils/supabaseFunctionsNew";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+
+import { useInView } from "react-intersection-observer";
 import { FiCameraOff } from "react-icons/fi";
 import { WiTime4 } from "react-icons/wi";
+import { IoIosArrowBack } from "react-icons/io";
+import { PiMicrophoneBold } from "react-icons/pi";
+
+import RecipeHeader from "@/app/conponents/RecipeHeader";
+import Footer from "@/app/conponents/Footer";
+import FavoriteButton from "@/app/conponents/FavoriteButton";
+import IngredientItem from "@/app/conponents/IngredientItem";
+import DescriptItem from "@/app/conponents/DescriptItem";
 import LoadingDataFetch from "@/app/conponents/LoadingDataFetch";
-import { useSwipeable } from "react-swipeable";
-import { deleteRecipeDatas } from "@/app/utils/supabaseFunctionsNew";
-import { useRouter } from "next/navigation";
+import { Descript, DetailRecipe, Ingredient } from "@/app/types";
+import { getDetailRecipebyId } from "@/app/utils/supabaseFunctionsNew";
 
 export default function RecipeId({
   params,
@@ -24,22 +26,11 @@ export default function RecipeId({
   params: { recipe_id: number };
   searchParams: { from?: string };
 }) {
-  const [list, setList] = useState<DetailRecipe>();
   const from = searchParams?.from || "/";
-  const [showFooter, setshowFooter] = useState(true);
-  const router = useRouter();
-
-  //スクロールを検知する
-  const handlers = useSwipeable({
-    onSwipedUp: () => setshowFooter(false),
-    onSwipedDown: () => setshowFooter(true),
-    delta: 60,
+  const [list, setList] = useState<DetailRecipe>();
+  const { ref, inView } = useInView({
+    threshold: 0,
   });
-
-  const deleter = async () => {
-    await deleteRecipeDatas(params.recipe_id);
-    router.replace("/");
-  };
 
   useEffect(() => {
     const getDetailRecipe = async () => {
@@ -51,24 +42,33 @@ export default function RecipeId({
 
   if (!list) {
     return (
-      <div className="flex justify-center items-center bg-[#FFFBF4] h-screen">
+      <div className="flex justify-center items-center bg-orange-primary h-screen">
         <LoadingDataFetch />
       </div>
     );
   }
   return (
-    <div {...handlers} className="bg-[#FFFBF4] contain-paint">
-      <title>{`${list.name} | てぶくろキッチン`}</title>
-      <RecipeHeader
-        bgColor="bg-white"
-        textColor="text-black"
-        title={list.name}
-        link={from === "favorites" ? "/favorites" : "/"}
-        iconFill="black"
-      />
+    <div className="bg-orange-primary contain-paint">
+      {!inView && (
+        <RecipeHeader
+          bgColor="bg-white"
+          textColor="text-black"
+          title={list.name}
+          link={from === "favorites" ? "/favorites" : "/"}
+          iconFill="black"
+        />
+      )}
 
       <main className="pb-10 min-h-[calc(100vh-150px)] ">
         <figure className="flex justify-center items-center border-b border-gray-400 shadow-md aspect-[3/2] bg-gray-100 relative">
+          {inView && (
+            <Link
+              href={from === "favorites" ? "/favorites" : "/"}
+              className="absolute top-4 left-4 text-3xl z-10 bg-white rounded-full p-1 shadow-lg cursor-pointer flex items-center justify-center"
+            >
+              <IoIosArrowBack />
+            </Link>
+          )}
           {/* nullのみを判定しているので、url先の画像が見つからない場合に対処できない */}
           {list.image_url ? (
             <Image
@@ -84,16 +84,23 @@ export default function RecipeId({
           )}
         </figure>
 
-        <div className="border-b border-gray-300 m-4 pb-4">
+        <div className="m-4 pb-2">
           <div className="flex justify-between gap-2">
             <div>
-              <p className="text-2xl font-semibold text-[#815B2C]">
+              <p ref={ref} className="text-2xl font-semibold text-orange-700">
                 {list.name}
               </p>
-              <p className="text-sm pt-1">by {list.profiles.name}</p>
-            </div>
-            <div className="bg-black cursor-pointer" onClick={deleter}>
-              delete
+              <div className="flex items-center gap-2 mt-2">
+                <Image
+                  src={list.profiles.avatar_url}
+                  alt={list.profiles.name}
+                  width={32}
+                  height={32}
+                  className="rounded-full"
+                  onError={() => console.error("Image failed to load")}
+                />
+                <p>{list.profiles.name}</p>
+              </div>
             </div>
             <FavoriteButton
               recipe={{
@@ -108,46 +115,45 @@ export default function RecipeId({
           </div>
 
           {list.time && (
-            <div className="flex items-center py-2 gap-2 text-black">
-              <WiTime4 fill="#fa003f" className="size-8" />
-              {list.time}
+            <div className="inline-flex items-center gap-1 rounded-full bg-orange-100 min-h-6 px-2 max-w h-auto mt-3">
+              <WiTime4 className="size-4 text-orange-500 shrink-0" />
+              <p className="text-xs text-orange-700 break-words">{list.time}</p>
             </div>
           )}
 
           {list.comment && (
-            <p className="text-sm font-semibold text-[#565656] pt-3">
-              {list.comment}
-            </p>
+            <p className="text-sm text-gray-600 pt-3">{list.comment}</p>
           )}
         </div>
 
         <div className="lg:flex items-start text-black">
-          <section className="pt-1 pb-8 flex-1 lg:max-w-md">
-            <div className="bg-[#F9DEDC] font-semibold text-sm px-4 py-2">
+          <section className="pb-7 flex-1 lg:max-w-md mx-4">
+            <div className="font-semibold text-lg text-orange-600  pb-1 border-b border-orange-600">
               {list.howmany ? <p>材料（{list.howmany}）</p> : <p>材料</p>}
             </div>
-            {list.ingredients.map((ingredient: Ingredient) => (
-              <IngredientItem
-                key={ingredient.id}
-                id={ingredient.id}
-                name={ingredient.name}
-                amount={ingredient.amount}
-                recipe_id={ingredient.recipe_id}
-              />
-            ))}
+            <div className="border-orange-200 border mt-4 rounded-xl bg-orange-secondary divide-y divide-dashed divide-neutral-400">
+              {list.ingredients.map((ingredient: Ingredient) => (
+                <IngredientItem
+                  key={ingredient.id}
+                  id={ingredient.id}
+                  name={ingredient.name}
+                  amount={ingredient.amount}
+                  recipe_id={ingredient.recipe_id}
+                />
+              ))}
+            </div>
           </section>
 
           <section className="mx-4 flex-1">
-            <p className="font-semibold text-lg pb-1 mb-3 border-b border-black">
+            <p className="font-semibold text-lg text-orange-600 pb-1 mb-4 border-b border-orange-600">
               作り方
             </p>
-            <div className="space-y-1">
+            <div className="space-y-3">
               {list.descripts.map((descript: Descript, index) => (
                 <DescriptItem
                   key={descript.id}
                   id={index + 1}
                   text={descript.text}
-                  // image_url={`https://picsum.photos/${descript.id + 500}`}
                   image_url={descript.image_url}
                   recipe_id={descript.recipe_id}
                 />
@@ -160,17 +166,14 @@ export default function RecipeId({
           href={`./${params.recipe_id}/cook${
             from === "favorites" ? "?from=favorites" : ""
           }`}
-          className="flex justify-center text-white bg-orange-400 hover:bg-orange-500 font-semibold rounded-xl text-lg py-3 w-64 shadow-md mx-auto mt-8"
+          className="flex justify-center items-center gap-1 text-white bg-orange-400 hover:bg-orange-500 font-semibold rounded-xl py-3 w-64 shadow-md mx-auto mt-8"
         >
-          つくる
+          <PiMicrophoneBold className="size-[18px]" />
+          調理を開始する
         </Link>
       </main>
-      <div
-        className={`sticky bottom-0 w-full z-20 transition-transform duration-200 ${
-          showFooter ? "translate-y-0" : "translate-y-full"
-        }`}
-      >
-        <Footer pathName="/registration" />
+      <div className="sticky bottom-0">
+        <Footer pathName="/" />
       </div>
     </div>
   );
--- src/app/conponents/DescriptItem.tsx
+++ src/app/conponents/DescriptItem.tsx
@@ -1,34 +1,30 @@
-import { Descript } from "@/app/types";
 import Image from "next/image";
 import { FiCameraOff } from "react-icons/fi";
+import { Descript } from "@/app/types";
 
-const DescriptItem = (props: Descript) => {
-  const { id, text, image_url } = props;
-
+const DescriptItem = ({ id, text, image_url }: Descript) => {
   return (
-    <div className="bg-white flex items-start justify-between p-1">
-      <div className="flex-shrink-0">
-        <p className="bg-orange-400 text-white size-4 flex items-center justify-center font-semibold text-xs rounded-sm">
+    <div className="bg-white flex items-start justify-between p-2 border-orange-200 border rounded-xl">
+      <div className="flex">
+        <p className="flex-shrink-0 bg-orange-400 text-white size-5 flex items-center justify-center font-semibold text-xs rounded-full">
           {id}
         </p>
-      </div>
-      <p className="pt-2 px-4 font-semibold text-xs break-words max-w-[calc(100%-90px)]">
-        {text}
-      </p>
-      {/* nullのみを判定しているので、url先の画像が見つからない場合に対処できない */}
 
+        <p className="px-2 text-sm break-words flex-grow">{text}</p>
+      </div>
+      {/* nullのみを判定しているので、url先の画像が見つからない場合に対処できない */}
       {image_url ? (
-        <div className="relative size-[72px]">
+        <div className="relative size-[72px] ">
           <Image
             src={image_url}
             alt={text || ""}
             fill={true}
-            className="my-auto object-cover"
+            className="my-auto object-cover rounded-xl"
             onError={() => console.error("Image failed to load")}
           />
         </div>
       ) : (
-        <div className="bg-gray-100 p-6">
+        <div className="bg-gray-100 p-6 rounded-xl">
           <FiCameraOff size={24} stroke="#737373" />
         </div>
       )}
