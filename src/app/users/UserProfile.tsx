0407d0a1cc0274d0d7232aeab6ee6869468af33c
akki-256 <168257172+akki-256@users.noreply.github.com>
2025-08-17 20:05:46 +0900

    update:タイポ

--- src/app/users/UserProfile.tsx
+++ src/app/users/UserProfile.tsx
@@ -2,19 +2,19 @@
 import Image from "next/image";
 import React from "react";
 
-const UserProfile = (profaile: { name: string; avatar: string }) => {
+const UserProfile = (profile: { name: string; avatar: string }) => {
   return (
     <div className="flex gap-x-3">
       <div className="relative size-16 shrink-0">
         <Image
-          src={profaile.avatar}
+          src={profile.avatar}
           alt="avatar_image"
           fill
           className="rounded-full object-cover"
         />
       </div>
       <div className="my-auto font-semibold">
-        <p className="pb-1 text-xl text-black">{profaile.name}</p>
+        <p className="pb-1 text-xl text-black">{profile.name}</p>
         {/* <p className="text-xs text-[#797575]">@1124awerq</p> */}
       </div>
     </div>
