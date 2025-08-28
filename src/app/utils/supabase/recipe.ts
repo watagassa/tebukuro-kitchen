973b1d17e20b8a8a42bceb4adaf64f92310f1e74
watagassa <135955629+watagassa@users.noreply.github.com>
2025-08-28 23:38:23 +0900

    fix:検索機能のID管理を改善し、キーワードごとに履歴を保持するように変更

--- src/app/utils/supabase/recipe.ts
+++ src/app/utils/supabase/recipe.ts
@@ -4,7 +4,7 @@ import { exchangeIDtoUUID } from "../supabaseLogin";
 import { getCurrentUserID } from "../supabaseFunctionsNew";
 
 const ramdomFetchedIds: number[] = [];
-const searchFetchedIds: number[] = [];
+const fetchedIdsMap = new Map<string, number[]>();
 let favoriteFetchedIds: number[] = [];
 let searchFavFeatchedIds: number[] = [];
 
@@ -26,15 +26,21 @@ export const randomFetcher = async () => {
 
 export const searchFeatcher = async (key: string) => {
   const kw = key.substring(key.indexOf("-") + 1, key.lastIndexOf("-"));
+
+  if (!fetchedIdsMap.has(kw)) {
+    fetchedIdsMap.set(kw, []);
+  }
+
   const { data, error } = await supabase.rpc("get_random_recipes_keyword", {
     count: 10,
-    exclude_ids: searchFetchedIds, // 取得済みID
+    exclude_ids: fetchedIdsMap.get(kw),
     keyword: kw,
   });
+
   if (error) throw error;
   if (data) {
     const newIds = data.map((r: Recipe) => r.id);
-    searchFetchedIds.push(...newIds);
+    fetchedIdsMap.get(kw)!.push(...newIds);
   }
   return data ?? ([] as Recipe[]);
 };
