3d9a905f4ccabea1c29714dd611241cf1e87070b
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-09-01 17:02:30 +0900

    レシピ終了ボタンの挙動修正

--- src/app/[recipe_id]/cook/Cook.tsx
+++ src/app/[recipe_id]/cook/Cook.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { SetStateAction, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
@@ -366,10 +365,10 @@ const Cook = ({
               </p>
             </button>
             {page == length - 1 ? (
-              <Link href={recipePage} className="font-bold">
+              <button onClick={() => setInCook(false)} className="font-bold">
                 <FaDoorOpen className="mx-7 my-1 mb-0 h-6 w-6" />
                 <div className="text-center">終了</div>
-              </Link>
+              </button>
             ) : (
               <button
                 onClick={() => {
