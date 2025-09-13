9eb95b074f534b44dd37f5e32207eedcffa91f0c
akki-256 <168257172+akki-256@users.noreply.github.com>
2025-09-13 09:33:37 +0900

    fix:マイページにてフッターが上に来てしまうバグを解決

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -17,21 +17,23 @@ const UserId = () => {
   if (!pageElements) return <Loading />;
 
   return (
-    <div className="flex min-h-screen flex-col bg-[#FFFBF4] text-black">
-      <UserHeader />
-      <section className="flex justify-between bg-white p-3">
-        <UserProfile
-          name={pageElements.profile.name}
-          avatar={pageElements.profile.avatar_url}
+    <div>
+      <div className="flex min-h-screen flex-col bg-[#FFFBF4] text-black">
+        <UserHeader />
+        <section className="flex justify-between bg-white p-3">
+          <UserProfile
+            name={pageElements.profile.name}
+            avatar={pageElements.profile.avatar_url}
+          />
+          <UserLogoutButton />
+        </section>
+        <p className="mb-4 mt-8 pl-2 text-xl font-semibold">マイレシピ一覧</p>
+        <UserRecipeList
+          user_id={pageElements.user_id}
+          userRecipe={pageElements.created_recipes}
+          pageType="self"
         />
-        <UserLogoutButton />
-      </section>
-      <p className="mb-4 mt-8 pl-2 text-xl font-semibold">マイレシピ一覧</p>
-      <UserRecipeList
-        user_id={pageElements.user_id}
-        userRecipe={pageElements.created_recipes}
-        pageType="self"
-      />
+      </div>
       <Footer pathName="/users" />
     </div>
   );
