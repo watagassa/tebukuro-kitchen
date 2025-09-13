e2e07c1766dcaf511d857906f8ddb9dbdf205710
akki-256 <168257172+akki-256@users.noreply.github.com>
2025-09-13 09:26:53 +0900

    fix:マイページ画面にて，フッターが上に来てしまうバグを解決

--- src/app/users/mypage/[user_id]/page.tsx
+++ src/app/users/mypage/[user_id]/page.tsx
@@ -30,26 +30,28 @@ const UserId = ({ params }: { params: { user_id: string } }) => {
   if (isLoading || !pageElements) return <Loading />;
 
   return (
-    <div className="flex min-h-screen flex-col bg-[#FFFBF4]">
-      <header className="sticky flex w-full items-center justify-center border-b border-gray-400 bg-orange-400 p-6 shadow-md">
-        <p className="text-balance pl-10 pr-4 text-center text-xl font-semibold text-white">
-          マイページ
-        </p>
-      </header>
-      <section className="flex justify-between bg-white p-3">
-        <UserProfile
-          name={pageElements.profile.name}
-          avatar={pageElements.profile.avatar_url}
-        />
-      </section>
-      <p className="mb-4 mt-8 pl-2 text-xl font-semibold">レシピ一覧</p>
-      <section className="mx-2 rounded-xl bg-white">
-        <UserRecipeList
-          user_id={params.user_id}
-          userRecipe={pageElements.created_recipes}
-          pageType="other"
-        />
-      </section>
+    <div>
+      <div className="flex min-h-screen flex-col bg-[#FFFBF4]">
+        <header className="sticky flex w-full items-center justify-center border-b border-gray-400 bg-orange-400 p-6 shadow-md">
+          <p className="text-balance pl-10 pr-4 text-center text-xl font-semibold text-white">
+            マイページ
+          </p>
+        </header>
+        <section className="flex justify-between bg-white p-3">
+          <UserProfile
+            name={pageElements.profile.name}
+            avatar={pageElements.profile.avatar_url}
+          />
+        </section>
+        <p className="mb-4 mt-8 pl-2 text-xl font-semibold">レシピ一覧</p>
+        <section className="mx-2 rounded-xl bg-white">
+          <UserRecipeList
+            user_id={params.user_id}
+            userRecipe={pageElements.created_recipes}
+            pageType="other"
+          />
+        </section>
+      </div>
       <Footer pathName="/" />
     </div>
   );
